87ced158667ee2de5e5fbb4d40d458584373e4c2
Andrey Usyaev <andrey.usyaev@gmail.com>
2025-09-05 00:30:29 +0300

    Add render entrypoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
-  
+
   namespace :api do
     namespace :v1 do
       post :convert_to_pdf, to: "svg#convert_to_pdf"
